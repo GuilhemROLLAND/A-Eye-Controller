b96d3c09292c79788101230b78d1acac074beda7
GuilhemROLLAND <guilhem99.rolland@gmail.com>
2022-05-18 15:52:06 +0200

    improve run_stockage with mode

--- A-Eye_Controller/StockagePC/run_stockage.py
+++ A-Eye_Controller/StockagePC/run_stockage.py
@@ -5,5 +5,9 @@ import stockage
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", type=str,required=True, help="name of the file")
+    parser.add_argument("-m", "--mode", type=str,required=False, help="mode of the capture")
     args = parser.parse_args()
-    stockage.placeImage(args.file)
+    if args.mode:
+        stockage.placeImage(img=args.file, mode=args.mode)
+    else:
+        stockage.placeImage(img=args.file)
